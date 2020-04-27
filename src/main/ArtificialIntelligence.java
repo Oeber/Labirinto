5b979734dba07d4637b949e25099b1fa914fa5cb
oeber <oeber@DESKTOP-P6VUGAK>
2020-04-27 20:14:26 -0300

    v1.5

--- src/main/ArtificialIntelligence.java
+++ src/main/ArtificialIntelligence.java
@@ -3,26 +3,64 @@ package main;
 import agent.AgentMaze;
 import environment.Maze;
 import general.PositionXY;
+import java.util.Scanner;
 
 public class ArtificialIntelligence {
 
-	public static void main(String[] args) throws InterruptedException {
-		
-		Maze maze = new Maze(4);
-		
-		maze.showMaze();
-		
-		AgentMaze agent = new AgentMaze(maze);
-		agent.setPosition(new PositionXY(2,2));
-		
-		while(agent.isNotEndEaten()) {
-			agent.zeroStack();
-			agent.move();
-			maze.showMaze();
-			Thread.sleep(1500);
-		}
-		return;
+    public static void main(String[] args) throws InterruptedException {
+        int posX, posY, mSize = -1;
+        boolean finish = true;
+        Scanner scan = new Scanner(System.in);
 
-	}
+        while (mSize < 0) {
+            System.out.println("Qual sera o tamanho do labirinto?");
+            mSize = scan.nextInt();
+            if (mSize < 0) {
+                System.out.println("Tamanho invalido");
+            }
+        }
+        while (finish) {
+            Maze maze = new Maze(mSize);
+            maze.showMaze();
+            AgentMaze agent = new AgentMaze(maze);
+            posX = -1;
+            posY = -1;
 
+            while (posX >= maze.getMazeSize() || posX < 0) {
+                System.out.println("Qual sera a posição inicial X do Astolfo:");
+                posX = scan.nextInt();
+                if (posX >= maze.getMazeSize() || posX < 0) {
+                    System.out.println("Posição invalida");
+                }
+            }
+            while (posY >= maze.getMazeSize() || posY < 0) {
+                System.out.println("Qual sera a posição inicial Y do Astolfo:");
+                posY = scan.nextInt();
+                if (posX >= maze.getMazeSize() || posX < 0) {
+                    System.out.println("Posição invalida");
+                }
+            }
+            agent.setPosition(new PositionXY(posX, posY));
+
+            while (agent.isNotEndEaten()) {
+                agent.zeroStack();
+                agent.move();
+                maze.showMaze();
+                Thread.sleep(1500);
+            }
+
+            if (!maze.isgoodSolution()) {
+                System.out.println("Solução não otima");
+                System.out.println("Deseja tentar outra posição inicial?");
+                System.out.println("1-Sim 2-Não");
+                if (scan.nextInt() != 1) {
+                    finish = false;
+                }
+
+            } else {
+                System.out.println("Solução otima");
+                finish = false;
+            }
+        }
+    }
 }
